dd5fb030d580fd1f8df5d650ed28af5879bd9452
Sevushka <artemev.seva@yandex.ru>
2025-04-19 11:23:23 +0300

    блаблаблабла
    вцфвцв

--- Assets/Scripts/Sheild.cs
+++ Assets/Scripts/Sheild.cs
@@ -6,11 +6,15 @@ public class Sheild : MonoBehaviour
 {
     public GameObject exploison;
     public int strenght=15;
+    public Camera camera;
+
     private void OnTriggerEnter(Collider other)
     {
         print(other.name);
         if ("SP_Rock06(Clone)" == other.name)
         {
+            camera.orthographicSize = 27.71f;
+            Time.timeScale = 0.1f;
             Instantiate(exploison, other.gameObject.transform.position, Quaternion.identity);
             GameObject.Destroy(other.gameObject);
             strenght -= 1;
@@ -22,9 +26,13 @@ public class Sheild : MonoBehaviour
 
     private void Update()
     {
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, 36.21f, 0.01f);
+        Time.timeScale = Mathf.Lerp(Time.timeScale,1,0.01f);
         if (strenght<0)
         {
             GameObject.Destroy(gameObject);
+            camera.orthographicSize = 36.21f;
+            Time.timeScale = 1f;
         }
         
     }
